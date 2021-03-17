e21c30d58679ca389631b29cca12fbeef47c2a2a
Emmanuel <emmanuel.rodriguez313@gmail.com>
2021-03-17 12:58:08 +0100

    adding header and navbar

--- header.php
+++ header.php
@@ -4,14 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/script.css" >
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+    
+    <title>Very Wild Trip</title>
 </head>
 
 <body>
-    <header>
-    
-    
-    
-    
-    
-    </header>
+<header>
+  <label class="hamburger" for="nav-toggle"></label>
+  <input id="nav-toggle" type="checkbox" class="hidden" />
+    <nav>
+        <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Votre Wild Trip !</a></li>
+        <li><a href="#">Nos voyages</a></li>
+        <li><a href="#">Qui sommes-nous ?</a></li>
+        </ul>
+    </nav>
+</header>
