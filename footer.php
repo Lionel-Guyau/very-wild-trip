0ce0a54e113cbac4b38794dc81f84cc0c7cfc2bc
Emmanuel <emmanuel.rodriguez313@gmail.com>
2021-03-17 14:55:04 +0100

    Up-dating header and footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-    <footer>
+    <footer class="footer">
     Very Wild Trip by <span>Léa,</span><span>Lionel,</span><span>Manu</span> and <span>Quentin !</span>
       <img src="images/WCS-logo.png" alt="Logo Wild Code School" />
     </footer>
--- index.php
+++ index.php
@@ -2,6 +2,29 @@
 include("header.php");
 ?>
 
+<h2>Notre Top 5 !</h2>
+    <section class="main-cards">
+      <div class="bigCard tiltEffect">
+        <h3>Voyage 1</h3>
+        <span id="Voyage1"></span>
+      </div>
+      <div class="bigCard tiltEffect">
+        <h3>Voyage 2</h3>
+        <span id="Voyage2"></span>
+      </div>
+      <div class="bigCard tiltEffect">
+        <h3>Voyage 3</h3>
+        <span id="Voyage3"></span>
+      </div>
+      <div class="bigCard tiltEffect">
+        <h3>Voyage 4</h3>
+        <span id="Voyage4"></span>
+      </div>
+      <div class="bigCard tiltEffect">
+        <h3>Voyage 5</h3>
+        <span id="Voyage5"></span>
+      </div>
+    </section>
 
 
 
