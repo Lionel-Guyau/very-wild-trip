664bf3f39ebb881c5340798f7e57d2c9036468e1
Emmanuel <emmanuel.rodriguez313@gmail.com>
2021-03-18 17:38:59 +0100

    adding some css in mainTrip.php

--- pagesComponents/mainTrip.php
+++ pagesComponents/mainTrip.php
@@ -3,12 +3,10 @@
 include('destinations_data.php');
 
 
-"<div class='picturesContainer'>";
 
-    foreach($destinations as $destination => $content) 
-    {
-        echo "<h2 class='picturesTitle'> $destination </h2> " . $content['image'];
-    }
 
-"</div>"; 
-    
+foreach($destinations as $destination => $content) 
+{
+        echo "<div class='picturesContainer'><h2 class='picturesTitle'> $destination </h2> " .$content['image'] . "</div>";
+}
+
