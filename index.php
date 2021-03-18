d36fad520274983cf2e94106d094c04d9985284c
leamahler85 <leamahler85@gmail.com>
2021-03-18 17:37:11 +0100

    Modifications continers top trip

--- index.php
+++ index.php
@@ -5,8 +5,11 @@ include("header.php");
 
 <section class="bg">
   <div class="content">
+    <div class="top1trip"></div>
+    <div class="top1trip"></div>
+    <div class="top1trip"></div>
+   
 
-  
   </div>
 
 
