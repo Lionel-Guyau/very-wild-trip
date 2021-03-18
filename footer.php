54e844b0d493ac9a623d00d359cfbf03d84d6a9a
Emmanuel RODRIGUEZ <61582649+Allukard31@users.noreply.github.com>
2021-03-18 13:53:27 +0100

    Revert "1er jet"

--- footer.php
+++ footer.php
@@ -1,9 +1,6 @@
     <footer class="footer">
-      <span>
-        <h3>Very Wild Trip</h3>
-      </span>:<span>Léa,</span><span>Lionel,</span><span>Manu</span> and <span>Quentin !</span>
-      <img src="images/wcs.png" alt="Logo Wild Code School" />
+    Very Wild Trip by <span>Léa,</span><span>Lionel,</span><span>Manu</span> and <span>Quentin !</span>
+      <img src="images/WCS-logo.png" alt="Logo Wild Code School" />
     </footer>
-    </body>
-
-    </html>
+</body>
+</html>
--- js/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById("hamburger");
-const navUL = document.getElementById("nav-ul");
-
-toggleButton.addEventListener('click', function() {
-	navUL.classList.toggle('show')
-});
