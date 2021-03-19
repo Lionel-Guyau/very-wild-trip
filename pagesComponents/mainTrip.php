437bbb73076526baacc4f67b65bbc70b062f7fed
Emmanuel <emmanuel.rodriguez313@gmail.com>
2021-03-19 10:20:58 +0100

    Yata the page trip.php is responsive git add -Agit add -A

--- pagesComponents/mainTrip.php
+++ pagesComponents/mainTrip.php
@@ -3,10 +3,11 @@
 include('destinations_data.php');
 
 
-
+echo "<div class ='tripPage'>";
 
 foreach($destinations as $destination => $content) 
 {
-        echo "<div class='picturesContainer'><h2 class='picturesTitle'> $destination </h2> " .$content['image'] . "</div>";
+        echo "<div class='picturesContainer'><h2> $destination </h2> " .$content['image'] . "</div>";
 }
 
+echo "<?div>";
