f406e5e78c7359734c22f858ca57c61d3586e2b7
unknown <quentin.burty@gmail.com>
2021-03-19 11:31:54 +0100

    Header and footer adjustment and presentation preparation

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ include("header.php");
 
 <section class="bg">
   <div class="content">
-    <div class="toptrips"></div>
-    <div class="toptrips"></div>
-    <div class="toptrips"></div>
+    <div class="toptrips"><img src="images/destinations_images/Toulouse.jpeg" alt=""></div>
+    <div class="toptrips"><img src="images/destinations_images/Paris.jpeg" alt=""></div>
+    <div class="toptrips"><img src="images/destinations_images/Pyongyang.jpeg" alt=""></div>
   </div>
 
 
