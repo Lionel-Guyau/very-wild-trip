10bd4a43a62e17eb34e76c85284501fc596d4580
Emmanuel <emmanuel.rodriguez313@gmail.com>
2021-03-20 16:00:43 +0100

    adding values (costByDay, changeOfScenery, wheather) in  array

--- pagesComponents/destinations_data.php
+++ pagesComponents/destinations_data.php
@@ -3,170 +3,194 @@
 $destinations = [
     'London' => [
         'image' => "<img src='../images/destinations_images/London.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''
+        'costByDay' => 120,
+        'changeOfScenery' => 1,
+        'wheather' => 'Humide',
+        'description' => '',
     ],
 
     'Berlin' => [
         'image' => "<img src='../images/destinations_images/Berlin.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''
+        'costByDay' => 100,
+        'changeOfScenery' => 1,
+        'wheather' => 'Froid',
+        'description' => '',
     ],
 
     'Barcelona' => [
         'image' => "<img src='../images/destinations_images/Barcelona.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 80,
+        'changeOfScenery' => 1,
+        'wheather' => 'Chaud',
+        'description' => '',    
     ],
 
     'Lisboa' => [
         'image' => "<img src='../images/destinations_images/Lisboa.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 70,
+        'changeOfScenery' => 1,
+        'wheather' => 'Chaud',
+        'description' => '',    
     ],
 
     'Paris' => [
         'image' => "<img src='../images/destinations_images/Paris.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 160,
+        'changeOfScenery' => 1,
+        'wheather' => 'Humide',
+        'description' => '',
     ],
 
     'Toulouse' => [
         'image' => "<img src='../images/destinations_images/Toulouse.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 90,
+        'changeOfScenery' => 1,
+        'wheather' => 'Chaud',
+        'description' => '',    
     ],
 
     'Dublin' => [
         'image' => "<img src='../images/destinations_images/Dublin.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 50,
+        'changeOfScenery' => 1,
+        'wheather' => 'Humide',
+        'description' => '',    
     ],
 
     'Tokyo' => [
         'image' => "<img src='../images/destinations_images/Tokyo.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 200,
+        'changeOfScenery' => 3,
+        'wheather' => 'Tempéré',
+        'description' => '',    
     ],
 
     'Pekin' => [
         'image' => "<img src='../images/destinations_images/Pekin.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 90,
+        'changeOfScenery' => 3,
+        'wheather' => 'Tempéré',
+        'description' => '',
     ],
 
     'Sydney' => [
         'image' => "<img src='../images/destinations_images/Sydney.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 150,
+        'changeOfScenery' => 2,
+        'wheather' => 'Chaud',
+        'description' => '',    
     ],
 
     'Moscov' => [
         'image' => "<img src='../images/destinations_images/Moscov.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 90,
+        'changeOfScenery' => 2,
+        'wheather' => 'Polaire',
+        'description' => '',    
     ],
 
     'Tarkov' => [
         'image' => "<img src='../images/destinations_images/Tarkov.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 0,
+        'changeOfScenery' => 3,
+        'wheather' => 'Polaire',
+        'description' => '',
     ],
 
     'Rio' => [
         'image' => "<img src='../images/destinations_images/Rio.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 50,
+        'changeOfScenery' => 2,
+        'wheather' => 'Chaud',
+        'description' => '',    
     ],
 
     'Los Angeles' => [
         'image' => "<img src='../images/destinations_images/Los_Angeles.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 250,
+        'changeOfScenery' => 2,
+        'wheather' => 'Chaud',
+        'description' => '',    
     ],
 
     'Cuba' => [
         'image' => "<img src='../images/destinations_images/Cuba.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 30,
+        'changeOfScenery' => 2,
+        'wheather' => 'Chaud',
+        'description' => '',    
     ],
 
     'Roma' => [
         'image' => "<img src='../images/destinations_images/Roma.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 150,
+        'changeOfScenery' => 1,
+        'wheather' => 'Tempéré',
+        'description' => '',    
     ],
 
     'Bruxelle' => [
         'image' => "<img src='../images/destinations_images/Bruxelle.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 100,
+        'changeOfScenery' => 1,
+        'wheather' => 'Humide',
+        'description' => '',    
     ],
 
     'Rabat' => [
         'image' => "<img src='../images/destinations_images/Rabat.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 40,
+        'changeOfScenery' => 2,
+        'wheather' => 'Aride',
+        'description' => '',    
     ],
 
     'Johannesburg' => [
         'image' => "<img src='../images/destinations_images/Johannesbourg.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 170,
+        'changeOfScenery' => 2,
+        'wheather' => 'Chaud',
+        'description' => '',    
     ],
 
     'Le Caire' => [
         'image' => "<img src='../images/destinations_images/Le_Caire.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 30,
+        'changeOfScenery' => 2,
+        'wheather' => 'Aride',
+        'description' => '',    
     ],
 
     'Ouagadougou' => [
         'image' => "<img src='../images/destinations_images/Ouagadougou.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 10,
+        'changeOfScenery' => 2,
+        'wheather' => 'Aride',
+        'description' => '',    
     ],
 
     'Bangkok' => [
         'image' => "<img src='../images/destinations_images/Bangkok.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 20,
+        'changeOfScenery' => 3,
+        'wheather' => 'Chaud',
+        'description' => '',    
     ],
 
     'Pyongyang' => [
         'image' => "<img src='../images/destinations_images/Pyongyang.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 10,
+        'changeOfScenery' => 3,
+        'wheather' => 'Humide',
+        'description' => '',    
     ],
 
     'Seoul' => [
         'image' => "<img src='../images/destinations_images/Seoul.jpeg/200/300'>",
-        'costByDay' => '',
-        'changeOfScenery' => '',
-        'wheather' => ''    
+        'costByDay' => 120,
+        'changeOfScenery' => 3,
+        'wheather' => 'Tempéré',
+        'description' => '',    
     ],
 
 
