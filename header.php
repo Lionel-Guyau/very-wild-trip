6bedaac8f33ddd410060fbb4b24287991078d5a7
unknown <quentin.burty@gmail.com>
2021-03-21 17:31:33 +0100

    Header + burger modified

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="css/formtrip.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Englebert&display=swap" rel="stylesheet">
 
     <title>Very Wild Trip</title>
 </head>
@@ -19,12 +20,11 @@
     <header>
         <label class="hamburger" for="nav-toggle"></label>
         <input id="nav-toggle" type="checkbox" class="hidden" />
-        <nav>
             <a href ="index.php" class="logo-button">
             <img class ="nav-logo" src="images/55a4640511d949dcb8dfc257f0a01890.png"
             </a>
+        <nav>
             <ul>
-                <li><a href="index.php">Accueil</a></li>
                 <li><a href="formtrip.php">Votre Wild Trip !</a></li>
                 <li><a href="trip.php">Nos voyages</a></li>
                 <li><a href="about.php">Qui sommes-nous ?</a></li>
