1d1532d037dfbacd83c693f834f17e465802196a
lionel-guyau <lguyau@gmail.com>
2021-03-21 16:38:50 +0100

    version13

--- formtrip.php
+++ formtrip.php
@@ -1,64 +1,62 @@
 <?php
 include("header.php");
-include('destinations_data.php');
+include("pagesComponents/destinations_data.php");
 ?>
+
 
 <!-----------------
 --    The Form   --
 ------------------>
 <div class="formtrip-body">
-    <form>
+    <form action="formtrip.php" method="POST">
         <div>
             <label for="inputTrip">Votre budget</label>
-            <select name="budget" id="selectBudget">
-                <option value="">En Euro jour/pers</option>
-                <option value="1">0€ - 150€</option>
-                <option value="2">150€ - 300€</option>
-                <option value="3">300€ - 1000€</option>
-            </select>
-
-
+            <input type="number" name="budget" id="inputTrip">
+            <!-- <option value="">En Euro jour/pers</option>
+            <option value="1">0€ - 150€</option>
+            <option value="2">150€ - 300€</option>
+            <option value="3">300€ - 1000€</option> -->
+            <!-- </select> -->
         </div>
         <div>
             <label for="inputDepaysement">Votre degré de dépaysement</label>
-            <select name="depay" id="selectDepay">
-                <option value="">Choix de 0 à 5</option>
-                <option value="1">0</option>
-                <option value="2">1</option>
-                <option value="3">2</option>
-                <option value="4">3</option>
+            <select name="depay" id="inputDepaysement">
+                <option value="">Choix de 0 à 3</option>
+                <option value=1>1</option>
+                <option value=2>2</option>
+                <option value=3>3</option>
             </select>
         </div>
         <div>
             <label for="inputClimat">Dans quel coin du globe</label>
-            <select name="climat" id="selectClimat">
+            <select name="climat" id="inputClimat">
                 <option value="">Choix du climat</option>
-                <option value="1">Froid</option>
-                <option value="2">Chaud</option>
-                <option value="3">Tempéré</option>
-                <option value="4">Polaire</option>
-                <option value="5">Humide</option>
-                <option value="6">Aride</option>
+                <option value=1>Froid</option>
+                <option value=2>Chaud</option>
+                <option value=3>Tempéré</option>
+                <option value=4>Polaire</option>
+                <option value=5>Humide</option>
+                <option value=6>Aride</option>
             </select>
         </div>
         <div>
             <label for="inputPerson">Nombre de personne</label>
-            <select name="person" id="selectPerson">
+            <select name="person" id="inputPerson">
                 <option value="">De 1 à 4</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
+                <option value=1>1</option>
+                <option value=2>2</option>
+                <option value=3>3</option>
+                <option value=4>4</option>
             </select>
         </div>
         <div>
             <label for="inputDuree">Quelle durée</label>
-            <select name="duree" id="selectDuree">
+            <select name="duree" id="inputDuree">
                 <option value="">En nombre de jour</option>
-                <option value="1">Week-end (2 jours)</option>
-                <option value="2">Semaine (7 jours)</option>
-                <option value="3">Semaine (14 jours)</option>
-                <option value="4">Mois (30 jours)</option>
+                <option value=2>Week-end (2 jours)</option>
+                <option value=7>Semaine (7 jours)</option>
+                <option value=14>Semaine (14 jours)</option>
+                <option value=30>Mois (30 jours)</option>
             </select>
         </div>
 
@@ -66,86 +64,34 @@ include('destinations_data.php');
     </form>
 
 
+
     <!----------------
 --   The cards   --
 ------------------>
 
+    <?php
+    echo "<div class ='tripPage'>";
 
-    <section class="main-cards">
+    foreach ($destinations as $destination => $content) {
+        echo "<div class='picturesContainer'>
+         <h2> $destination </h2> " .
+            "<div class='destinationImage'>" .
+            $content['image'] .
+            "</div>
+                <div class='descriptionParagraph'>" .
+            $content['description'] .
+            "</div>                
+        </div>";
+    }
 
-        <?php if ($_GET['budget'] == 1) { ?>
-            <div class="card-trip">
-                <div>
-                    <img src="images/destinations_images/Bruxelle.jpeg" width="300px" height="300px">
-                </div>
-            </div>
-            <div class="card-trip">
-                <div>
-                    <img src="images/destinations_images/Paris.jpeg" width="300px" height="300px">
-                </div>
-            </div>
-            <div class="card-trip">
-                <div>
-                    <img src="images/destinations_images/Toulouse.jpeg" width="300px" height="300px">
-                </div>
-            </div>
-        <?php } ?>
+    echo "<?div>";
+    ?>
 
+    <?php
 
-        <?php if ($_GET['budget'] == 2) { ?>
-            <div class="card-trip">
-                <div>
-                    <img src="images/destinations_images/Berlin.jpeg" width="300px" height="300px">
-                </div>
-            </div>
-            <div class="card-trip">
-                <div>
-                    <img src="images/destinations_images/London.jpeg" width="300px" height="300px">
-                </div>
-            </div>
-            <div class="card-trip">
-                <div>
-                    <img src="images/destinations_images/Roma.jpeg" width="300px" height="300px">
-                </div>
-            </div>
+    foreach ($destinations as $destination => $content) {
 
-
-        <?php } ?>
-
-
-        <?php if ($_GET['climat'] == 2) { ?>
-            <div class="card-trip">
-                <div>
-                    <img src="images/destinations_images/Rio.jpeg" width="300px" height="300px">
-                </div>
-            </div>
-            <div class="card-trip">
-                <div>
-                    <img src="images/destinations_images/Sydney.jpeg" width="300px" height="300px">
-                </div>
-            </div>
-            <div class="card-trip">
-                <div>
-                    <img src="images/destinations_images/Cuba.jpeg" width="300px" height="300px">
-                </div>
-            </div>
-
-
-        <?php } ?>
-
-
-        <?php if ($_GET['climat'] == 4) { ?>
-            <div class="card-trip">
-                <div>
-                    <img src="images/destinations_images/Moscov.jpeg" width="300px" height="300px">
-                </div>
-            </div>
-        <?php } ?>
-
-    </section>
-</div>
-
-
-<?php
-include("footer.php");
-?>
+        if ($_POST['climat'] === $content['wheather'] && $_POST['depay'] === $content['changeOfScenery'] && $_POST['budget'] >= $content['costByDay'] * $_POST['duree'] * $_POST['person']) {
+        }
+    }
+    ?>
--- pagesComponents/destinations_data.php
+++ pagesComponents/destinations_data.php
@@ -13,7 +13,7 @@ Wheather <=> inputClimat corrrespondance :
 $destinations = [
     'London' => [
         'image' => "<img src='../images/destinations_images/London.jpeg/200/300'>",
-        'costByDay' => 120,
+        'costByDay' => 1,
         'changeOfScenery' => 1,
         'wheather' => 5,
         'description' => "<p>Venez visiter les attractions d’envergure internationale de Londres, ainsi que ses magasins et ses restaurants, et venez vibrer au rythme des spectacles extraordinaires et des pièces de théâtre, sans oublier les concerts, les expositions d’art, etc. Découvrez avec nous ce que Londres a de mieux à vous proposer.</p>",
@@ -32,7 +32,7 @@ $destinations = [
         'costByDay' => 80,
         'changeOfScenery' => 1,
         'wheather' => 2,
-        'description' => "<p>Barcelone propose une multitude d’activités de loisirs qui invitent ses visiteurs à y venir plus d’une fois. Ouverte sur la mer Méditerranée et célèbre pour Gaudí et son architecture moderniste, Barcelone est l’une des capitales européennes les plus trendy.</p>",    
+        'description' => "<p>Barcelone propose une multitude d’activités de loisirs qui invitent ses visiteurs à y venir plus d’une fois. Ouverte sur la mer Méditerranée et célèbre pour Gaudí et son architecture moderniste, Barcelone est l’une des capitales européennes les plus trendy.</p>",
     ],
 
     'Lisbone' => [
@@ -40,7 +40,7 @@ $destinations = [
         'costByDay' => 70,
         'changeOfScenery' => 1,
         'wheather' => 2,
-        'description' => "<p>Nichée au milieu de sept collines, cette immense ville piétonne offre une alternative enchanteresse aux capitales les plus populaires d’Europe. Avec une vie nocturne animée, des marchés festifs et des musées dynamiques, Lisbonne offre de nombreuses options pour prendre quelques verres de porto, une généreuse portion de bacalhau et une bonne quantité de pastéis de nata.</p>",    
+        'description' => "<p>Nichée au milieu de sept collines, cette immense ville piétonne offre une alternative enchanteresse aux capitales les plus populaires d’Europe. Avec une vie nocturne animée, des marchés festifs et des musées dynamiques, Lisbonne offre de nombreuses options pour prendre quelques verres de porto, une généreuse portion de bacalhau et une bonne quantité de pastéis de nata.</p>",
     ],
 
     'Paris' => [
@@ -56,7 +56,7 @@ $destinations = [
         'costByDay' => 90,
         'changeOfScenery' => 1,
         'wheather' => 2,
-        'description' => "<p>Toulouse est la capitale de l’Occitanie. Chaleureuse, spontanée, passionnée, elle vous accueille dans son écrin de briques orangées, façonné par 2 000 ans d’histoire. Grands monuments et lieux dédiés à la culture aéronautique et spatiale s’y côtoient avec bonheur. Le soleil, le sens de la fête, la douceur de vivre s’y mélangent, pour en faire la ville où beaucoup de Français rêvent de venir s’installer.</p>",    
+        'description' => "<p>Toulouse est la capitale de l’Occitanie. Chaleureuse, spontanée, passionnée, elle vous accueille dans son écrin de briques orangées, façonné par 2 000 ans d’histoire. Grands monuments et lieux dédiés à la culture aéronautique et spatiale s’y côtoient avec bonheur. Le soleil, le sens de la fête, la douceur de vivre s’y mélangent, pour en faire la ville où beaucoup de Français rêvent de venir s’installer.</p>",
     ],
 
     'Dublin' => [
@@ -64,7 +64,7 @@ $destinations = [
         'costByDay' => 50,
         'changeOfScenery' => 1,
         'wheather' => 5,
-        'description' => "<p>Que vous soyez à la recherche d’une bonne bière brune, d’un véritable fish & chips ou bien d’une expérience très irlandaise, Dublin est la ville où s’arrêter. Il est recommandé d’y passer bien plus qu’une journée, afin d’avoir l’opportunité de cerner l’ensemble de la cité.</p>",    
+        'description' => "<p>Que vous soyez à la recherche d’une bonne bière brune, d’un véritable fish & chips ou bien d’une expérience très irlandaise, Dublin est la ville où s’arrêter. Il est recommandé d’y passer bien plus qu’une journée, afin d’avoir l’opportunité de cerner l’ensemble de la cité.</p>",
     ],
 
     'Tokyo' => [
@@ -72,7 +72,7 @@ $destinations = [
         'costByDay' => 200,
         'changeOfScenery' => 3,
         'wheather' => 3,
-        'description' => "<p>La capitale du Japon est la ville la plus peuplée au monde. Tokyo est immense et offre de nombreuses possibilités de découvertes que ce soit gustatives, culturelles ou historiques. Ses 23 arrondissements sont arrivés à garder un esprit villageois au sein de cette mégalopole gigantesque. A n'importe quelle heure du jour et de la nuit, vous vous sentirez en sécurité et pourrez vous balader à pied bien que les distances soient conséquentes.</p>",    
+        'description' => "<p>La capitale du Japon est la ville la plus peuplée au monde. Tokyo est immense et offre de nombreuses possibilités de découvertes que ce soit gustatives, culturelles ou historiques. Ses 23 arrondissements sont arrivés à garder un esprit villageois au sein de cette mégalopole gigantesque. A n'importe quelle heure du jour et de la nuit, vous vous sentirez en sécurité et pourrez vous balader à pied bien que les distances soient conséquentes.</p>",
     ],
 
     'Pekin' => [
@@ -88,7 +88,7 @@ $destinations = [
         'costByDay' => 150,
         'changeOfScenery' => 2,
         'wheather' => 2,
-        'description' => "<p>Non, Sydney n'est pas la capitale de l'Australie, mais elle est à coup sûr le ville la plus attractive du pays : pour son site, ses plages, son activité, son ambiance et sa dimension culturelle, Sydney reste l'étape incontournable de tout voyage australien.</p>",    
+        'description' => "<p>Non, Sydney n'est pas la capitale de l'Australie, mais elle est à coup sûr le ville la plus attractive du pays : pour son site, ses plages, son activité, son ambiance et sa dimension culturelle, Sydney reste l'étape incontournable de tout voyage australien.</p>",
     ],
 
     'Moscou' => [
@@ -96,7 +96,7 @@ $destinations = [
         'costByDay' => 90,
         'changeOfScenery' => 2,
         'wheather' => 4,
-        'description' => "<p>Un séjour dans la capitale russe est également un voyage entre la Russie des Tsars, la période communiste et la Russie contemporaine, qui a accroché le wagon de l’économie capitaliste au début des années 90. Pourquoi visiter Moscou ? Car elle se visite au fil de ses musées et de son architecture typique !</p>",    
+        'description' => "<p>Un séjour dans la capitale russe est également un voyage entre la Russie des Tsars, la période communiste et la Russie contemporaine, qui a accroché le wagon de l’économie capitaliste au début des années 90. Pourquoi visiter Moscou ? Car elle se visite au fil de ses musées et de son architecture typique !</p>",
     ],
 
     'Tarkov' => [
@@ -112,7 +112,7 @@ $destinations = [
         'costByDay' => 50,
         'changeOfScenery' => 2,
         'wheather' => 2,
-        'description' => "<p>Avec près de 13 millions d'habitants dans son agglomération, Rio de Janeiro est l'une des villes les plus importantes du continent américain, et c'est la vitrine du Brésil. Ville la plus visitée du Brésil, Rio est surnommée la Cidade Maravilhosa, notamment pour sa situation dans un cadre naturel superbe. D'un côté l'Océan Atlantique, les plages d'Ipanema, de Copacabana, le Pain de Sucre et la baie de Guanabara, de l'autre la montagne du Corcovado et son Christ Rédempteur. La cité carioca est festive et animée, et il est difficile de s'y ennuyer !</p>",    
+        'description' => "<p>Avec près de 13 millions d'habitants dans son agglomération, Rio de Janeiro est l'une des villes les plus importantes du continent américain, et c'est la vitrine du Brésil. Ville la plus visitée du Brésil, Rio est surnommée la Cidade Maravilhosa, notamment pour sa situation dans un cadre naturel superbe. D'un côté l'Océan Atlantique, les plages d'Ipanema, de Copacabana, le Pain de Sucre et la baie de Guanabara, de l'autre la montagne du Corcovado et son Christ Rédempteur. La cité carioca est festive et animée, et il est difficile de s'y ennuyer !</p>",
     ],
 
     'Los Angeles' => [
@@ -120,7 +120,7 @@ $destinations = [
         'costByDay' => 250,
         'changeOfScenery' => 2,
         'wheather' => 2,
-        'description' => "<p>Elle porte en elle une bonne part du rêve américain. Symbole de la Conquête de l’Ouest, elle est le point d’arrivée de la Route 66. Avec 320 jours de soleil par an au compteur, il y fait presque toujours un temps radieux !</p>",    
+        'description' => "<p>Elle porte en elle une bonne part du rêve américain. Symbole de la Conquête de l’Ouest, elle est le point d’arrivée de la Route 66. Avec 320 jours de soleil par an au compteur, il y fait presque toujours un temps radieux !</p>",
     ],
 
     'Cuba' => [
@@ -128,7 +128,7 @@ $destinations = [
         'costByDay' => 30,
         'changeOfScenery' => 2,
         'wheather' => 2,
-        'description' => "<p>Cuba, c’est mode de vie unique au Nouveau Monde ! Ses kilomètres de plages bordées de récifs coralliens ont la couleur de la nacre, ses chaînes de montagnes se teintent du vert profond des forêts tropicales, ses villes se parent d’une architecture hispano-coloniale aux couleurs souvent débridées, et son peuple, comme aspiré dans une parenthèse temporelle, entre belles américaines et carrioles à cheval, ne lasse pas de séduire...</p>",    
+        'description' => "<p>Cuba, c’est mode de vie unique au Nouveau Monde ! Ses kilomètres de plages bordées de récifs coralliens ont la couleur de la nacre, ses chaînes de montagnes se teintent du vert profond des forêts tropicales, ses villes se parent d’une architecture hispano-coloniale aux couleurs souvent débridées, et son peuple, comme aspiré dans une parenthèse temporelle, entre belles américaines et carrioles à cheval, ne lasse pas de séduire...</p>",
     ],
 
     'Rome' => [
@@ -136,7 +136,7 @@ $destinations = [
         'costByDay' => 150,
         'changeOfScenery' => 1,
         'wheather' => 3,
-        'description' => "<p>Rome est connue comme la 'Ville Éternelle' puisque le temps semble s'être arrêté il y a des siècles de cela. Ses monuments et ses vestiges d'édifices imposants transforment une promenade dans ses rues en un voyage dans le temps jusqu'à l'époque de grande splendeur de la capitale.</p>",    
+        'description' => "<p>Rome est connue comme la 'Ville Éternelle' puisque le temps semble s'être arrêté il y a des siècles de cela. Ses monuments et ses vestiges d'édifices imposants transforment une promenade dans ses rues en un voyage dans le temps jusqu'à l'époque de grande splendeur de la capitale.</p>",
     ],
 
     'Bruxelle' => [
@@ -144,7 +144,7 @@ $destinations = [
         'costByDay' => 100,
         'changeOfScenery' => 1,
         'wheather' => 5,
-        'description' => "<p>Bruxelles la belle, capitale de la Belgique et siège des institutions européennes, constitue la destination idéale pour un city-break ou des vacances plus longues. Enclave francophone et wallonne en territoire flamand, elle accueille également 30% d'étrangers qui y ont posé leurs valises à l'année.</p>",    
+        'description' => "<p>Bruxelles la belle, capitale de la Belgique et siège des institutions européennes, constitue la destination idéale pour un city-break ou des vacances plus longues. Enclave francophone et wallonne en territoire flamand, elle accueille également 30% d'étrangers qui y ont posé leurs valises à l'année.</p>",
     ],
 
     'Rabat' => [
@@ -152,7 +152,7 @@ $destinations = [
         'costByDay' => 40,
         'changeOfScenery' => 2,
         'wheather' => 6,
-        'description' => "<p>Envie de découvrir Rabat ? Il y a mille et une façons de le vivre. Une ville côtière, son littoral déroule près de 60 kilomètres de côtes où se succèdent criques rocheuses ou plages de sable fin et son arrière-pays offre une diversité écologique idéale pour les randonnées.</p>",    
+        'description' => "<p>Envie de découvrir Rabat ? Il y a mille et une façons de le vivre. Une ville côtière, son littoral déroule près de 60 kilomètres de côtes où se succèdent criques rocheuses ou plages de sable fin et son arrière-pays offre une diversité écologique idéale pour les randonnées.</p>",
     ],
 
     'Johannesburg' => [
@@ -160,7 +160,7 @@ $destinations = [
         'costByDay' => 170,
         'changeOfScenery' => 2,
         'wheather' => 2,
-        'description' => "<p>Comment ne pas être séduit par l'énergie qui se dégage des rues de Johannesburg. La jeunesse apporte un nouveau souffle aux quartiers en développant une scène artistique dynamique dans de nombreux domaines, que ce soit la mode, la photographie ou la musique. </p>",    
+        'description' => "<p>Comment ne pas être séduit par l'énergie qui se dégage des rues de Johannesburg. La jeunesse apporte un nouveau souffle aux quartiers en développant une scène artistique dynamique dans de nombreux domaines, que ce soit la mode, la photographie ou la musique. </p>",
     ],
 
     'Le Caire' => [
@@ -168,7 +168,7 @@ $destinations = [
         'costByDay' => 30,
         'changeOfScenery' => 2,
         'wheather' => 6,
-        'description' => "<p>Partez à la découverte de la terre des pharaons en vous offrant un vol vers Le Caire. Mystères, mythes et légendes se mélangent dans cette capitale résolument moderne. Souks et sarcophages, papyrus et pyramides, momies et musées, vous n'avez pas fini de découvrir tous les aspects de cette grande ville, la plus grande d'Égypte et du Moyen Orient!</p>",    
+        'description' => "<p>Partez à la découverte de la terre des pharaons en vous offrant un vol vers Le Caire. Mystères, mythes et légendes se mélangent dans cette capitale résolument moderne. Souks et sarcophages, papyrus et pyramides, momies et musées, vous n'avez pas fini de découvrir tous les aspects de cette grande ville, la plus grande d'Égypte et du Moyen Orient!</p>",
     ],
 
     'Ouagadougou' => [
@@ -176,7 +176,7 @@ $destinations = [
         'costByDay' => 10,
         'changeOfScenery' => 2,
         'wheather' => 6,
-        'description' => "<p>Ouagadougou se présente comme un véritable phare de la culture africaine, dressé en plein centre du Burkina Faso. Pourtant, son intérêt est limité pour le voyageur car il y a peu à visiter. Le charme de Ouagadougou réside dans son atmosphère de capitale africaine affairée et bruyante.</p>",    
+        'description' => "<p>Ouagadougou se présente comme un véritable phare de la culture africaine, dressé en plein centre du Burkina Faso. Pourtant, son intérêt est limité pour le voyageur car il y a peu à visiter. Le charme de Ouagadougou réside dans son atmosphère de capitale africaine affairée et bruyante.</p>",
     ],
 
     'Bangkok' => [
@@ -184,7 +184,7 @@ $destinations = [
         'costByDay' => 15,
         'changeOfScenery' => 3,
         'wheather' => 2,
-        'description' => "<p>Bangkok ne dort jamais, c'est l'une des villes les plus visitées au monde et elle est très facilement accessible en avion, dont le billet est de moins en moins cher. Il n'est pas étonnant que le voyageur arrive la valise vide pour y faire ses achats de vêtements comme d'électroniques.</p>",    
+        'description' => "<p>Bangkok ne dort jamais, c'est l'une des villes les plus visitées au monde et elle est très facilement accessible en avion, dont le billet est de moins en moins cher. Il n'est pas étonnant que le voyageur arrive la valise vide pour y faire ses achats de vêtements comme d'électroniques.</p>",
     ],
 
     'Pyongyang' => [
@@ -192,7 +192,7 @@ $destinations = [
         'costByDay' => 10,
         'changeOfScenery' => 3,
         'wheather' => 5,
-        'description' => "<p>Pays difficile d’accès et peu engageant au premier abord, la Corée du Nord a pourtant un potentiel touristique. Venez le découvrir !</p>",    
+        'description' => "<p>Pays difficile d’accès et peu engageant au premier abord, la Corée du Nord a pourtant un potentiel touristique. Venez le découvrir !</p>",
     ],
 
     'Seoul' => [
@@ -200,7 +200,7 @@ $destinations = [
         'costByDay' => 120,
         'changeOfScenery' => 3,
         'wheather' => 3,
-        'description' => "<p>Comme toutes les villes d'Asie, Séoul cultive un équilibre particulier entre passé et modernité. Si la capitale de la Corée du Sud et ses immenses buildings témoignent du formidable dynamisme économique du pays depuis trente ans, elle a gardé l'empreinte de son histoire et possède une culture très riche qui sait séduire quand on prend le temps de la découvrir.</p>",    
+        'description' => "<p>Comme toutes les villes d'Asie, Séoul cultive un équilibre particulier entre passé et modernité. Si la capitale de la Corée du Sud et ses immenses buildings témoignent du formidable dynamisme économique du pays depuis trente ans, elle a gardé l'empreinte de son histoire et possède une culture très riche qui sait séduire quand on prend le temps de la découvrir.</p>",
     ],
 
 
