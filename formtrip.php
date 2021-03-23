64ac14d007bedea455a0acfc32465210e96a0dba
Emmanuel RODRIGUEZ <emmanuel.rodriguez313@gmail.com>
2021-03-23 17:10:22 +0100

    Delete useless comment in formTrip.php and updating trip.css

--- formtrip.php
+++ formtrip.php
@@ -74,24 +74,6 @@ include("pagesComponents/destinations_data.php");
 --   The cards   --
 ------------------>
 
-    <!-- <?php
-            echo "<div class ='tripPage'>";
-
-            foreach ($destinations as $destination => $content) {
-                echo "<div class='picturesContainer'>
-         <h2> $destination </h2> " .
-                    "<div class='destinationImage'>" .
-                    $content['image'] .
-                    "</div>
-                <div class='descriptionParagraph'>" .
-                    $content['description'] .
-                    "</div>                
-        </div>";
-            }
-
-            echo "<?div>";
-            ?> -->
-
     <?php
      
 
--- pagesComponents/mainTrip.php
+++ pagesComponents/mainTrip.php
@@ -4,18 +4,21 @@ include('destinations_data.php');
 
 
 echo "<div class ='tripPage'>";
+echo "<p class='tripPageDescription'>Vous trouverez ici toutes nos déstinations proposées par nos partenaires à travers le mondes, n'hésités pas à vous balader parmis ces déstinations de choix.</p>";
 
-foreach($destinations as $destination => $content) 
-{
-echo "<div class='picturesContainer'>
-         <h2> $destination </h2> " .    
-                "<div class='destinationImage'>" .
-                        $content['image'] . 
-                "</div>
-                <div class='descriptionParagraph'>" . 
-                        $content['description'] . 
-                "</div>                
-        </div>";    
-}
+        
+
+        foreach($destinations as $destination => $content) 
+        {
+            echo"<div class='picturesContainer'>
+                        <h2> $destination </h2> " .    
+                                "<div class='destinationImage'>" .
+                                        $content['image'] . 
+                                "</div>
+                                <div class='descriptionParagraph'>" . 
+                                        $content['description'] . 
+                                "</div>                
+                </div>";    
+        }
 
 echo "<?div>";
