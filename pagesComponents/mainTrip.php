e41ad9af0152ef242b425d9b8825e90c6b61704d
Emmanuel RODRIGUEZ <emmanuel.rodriguez313@gmail.com>
2021-03-24 16:22:56 +0100

    last update tripPage

--- pagesComponents/mainTrip.php
+++ pagesComponents/mainTrip.php
@@ -3,7 +3,7 @@
 include('destinations_data.php');
 
 shuffle($destinations);
-$destinations = array_slice($destinations, 0, 9);
+$destinations = array_slice($destinations, 0, 5);
 echo "<div class ='tripPage'>";
 echo "<p class='tripPageDescription'>Vous trouverez ici un échantillon de nos déstinations proposées par nos partenaires à travers le monde. </br> Rendez-vous sur notre page \"Votre Wild Trip\" afin de trouver la destination de vos rêves !!!</br>Vous pouvez <a href='trip.php'>rafraîchir la page</a> si vous souhaiter voir un autre échantillons de nos offres.</p>";
 
@@ -17,4 +17,5 @@ echo "<p class='tripPageDescription'>Vous trouverez ici un échantillon de nos d
                         </div>";
         }
          
+echo "<div class='bottomButton'><a href='trip.php'>rafraîchir la page</a></div>";
 echo "</div>";
