0a5f9beb8afeaecf61797a95f36e7d506408b3c1
Emmanuel RODRIGUEZ <emmanuel.rodriguez313@gmail.com>
2021-03-24 16:12:19 +0100

    finish refresh button

--- pagesComponents/mainTrip.php
+++ pagesComponents/mainTrip.php
@@ -5,7 +5,7 @@ include('destinations_data.php');
 shuffle($destinations);
 $destinations = array_slice($destinations, 0, 9);
 echo "<div class ='tripPage'>";
-echo "<p class='tripPageDescription'>Vous trouverez ici un échantillon de nos ddstinations proposées par nos partenaires à travers le monde. </br> Rendez-vous sur notre page \"Votre Wild Trip\" afin de trouver la destination de vos rêves !!!</br><a href='trip.php'>rafréchir la page</a></p>";
+echo "<p class='tripPageDescription'>Vous trouverez ici un échantillon de nos déstinations proposées par nos partenaires à travers le monde. </br> Rendez-vous sur notre page \"Votre Wild Trip\" afin de trouver la destination de vos rêves !!!</br>Vous pouvez <a href='trip.php'>rafraîchir la page</a> si vous souhaiter voir un autre échantillons de nos offres.</p>";
 
         
         foreach($destinations as $destination => $content) 
