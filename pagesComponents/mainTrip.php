4fa5294f3a84ff39c47bd38a3f2469bd3f0a2d44
Emmanuel RODRIGUEZ <emmanuel.rodriguez313@gmail.com>
2021-03-24 10:40:07 +0100

    complete trip.php aleatory and limit of 10 cards on screen

--- pagesComponents/mainTrip.php
+++ pagesComponents/mainTrip.php
@@ -3,19 +3,18 @@
 include('destinations_data.php');
 
 shuffle($destinations);
-
+$destinations = array_slice($destinations, 0, 10);
 echo "<div class ='tripPage'>";
 echo "<p class='tripPageDescription'>Vous trouverez ici toutes nos déstinations proposées par nos partenaires à travers le mondes, n'hésités pas à vous balader parmis ces déstinations de choix.</p>";
 
-
+        
         foreach($destinations as $destination => $content) 
-        {
-            echo        "<div class='picturesContainer'>
+        {                        
+                echo    "<div class='picturesContainer'>
                                 {$content['name']} 
-                                <div class='destinationImage'> {$content['image']}</div>
+                                <div class='destinationImage'>{$content['image']}</div>
                                 <div class='descriptionParagraph'>{$content['description']}</div>               
                         </div>";
-                        
         }
-
+         
 echo "</div>";
