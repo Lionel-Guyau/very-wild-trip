4253b9e14bf417cc9b4f6117cccd22f1e3cb4a08
lionel-guyau <lguyau@gmail.com>
2021-03-24 14:40:57 +0100

    version34

--- formtrip.php
+++ formtrip.php
@@ -8,6 +8,31 @@ $depay = isset($_POST['depay']) ? $_POST['depay'] : '';
 $wheather = isset($_POST['climat']) ? $_POST['climat'] : '';
 $person = isset($_POST['person']) ? $_POST['person'] : '';
 $duree = isset($_POST['duree']) ? $_POST['duree'] : '';
+
+$filteredDestinations = [];
+
+// Application des critères sur la liste de voyage
+foreach ($destinations as $destination => $content) {
+    if (!empty($budget) && !empty($person) && !empty($duree) && $budget < $content['costByDay'] * $duree * $person) {
+        continue;
+    }
+
+    if (!empty($wheather) && $wheather !== $content['wheather']) {
+        continue;
+    }
+
+    if (!empty($depay) && $depay != $content['changeOfScenery']) {
+        continue;
+    }
+
+    $content['total'] = $content['costByDay'] * $person * $duree;
+    $filteredDestinations[$destination] = $content;
+}
+
+// Si aucun critère n'a été renseigné, on affiche tous les voyages
+if (empty($_POST)) {
+    $filteredDestinations = $destinations;
+}
 ?>
 
 
@@ -22,7 +47,7 @@ $duree = isset($_POST['duree']) ? $_POST['duree'] : '';
         </div>
         <div>
             <label for="inputDepaysement">Votre degré de dépaysement</label>
-            <select name="depay" id="inputDepaysement" required>
+            <select name="depay" id="inputDepaysement">
                 <option value="">Choix de 1 à 3</option>
                 <option value="1" <?= $depay == '1' ? 'selected' : '' ?>>1</option>
                 <option value="2" <?= $depay == '2' ? 'selected' : '' ?>>2</option>
@@ -31,7 +56,7 @@ $duree = isset($_POST['duree']) ? $_POST['duree'] : '';
         </div>
         <div>
             <label for="inputClimat">Chois du climat</label>
-            <select name="climat" id="inputClimat" required>
+            <select name="climat" id="inputClimat">
                 <option value="">Choix du climat</option>
                 <option value="Froid" <?= $wheather == 'Froid' ? 'selected' : '' ?>>Froid</option>
                 <option value="Chaud" <?= $wheather == 'Chaud' ? 'selected' : '' ?>>Chaud</option>
@@ -43,7 +68,7 @@ $duree = isset($_POST['duree']) ? $_POST['duree'] : '';
         </div>
         <div>
             <label for="inputPerson">Nombre de personne</label>
-            <select name="person" id="inputPerson" required>
+            <select name="person" id="inputPerson">
                 <option value="">De 1 à 4</option>
                 <option value="1" <?= $person == '1' ? 'selected' : '' ?>>1</option>
                 <option value="2" <?= $person == '2' ? 'selected' : '' ?>>2</option>
@@ -53,7 +78,7 @@ $duree = isset($_POST['duree']) ? $_POST['duree'] : '';
         </div>
         <div>
             <label for="inputDuree">Quelle durée</label>
-            <select name="duree" id="inputDuree" required>
+            <select name="duree" id="inputDuree">
                 <option value="">En nombre de jour</option>
                 <option value="2" <?= $duree == '2' ? 'selected' : '' ?>>Week-end (2 jours)</option>
                 <option value="7" <?= $duree == '7' ? 'selected' : '' ?>>Semaine (7 jours)</option>
@@ -65,40 +90,54 @@ $duree = isset($_POST['duree']) ? $_POST['duree'] : '';
         <input type="submit" id="submit" value="Mes choix" />
 
     </form>
-    <p class="formtrip-textright">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ratione molestiae, commodi veniam,
-        dolores non hic deleniti blanditiis explicabo aut, voluptatibus labore vel atque reiciendis velit mollitia rerum maxime delectus.</p>
+
+    <h2>
+        <?php if (!empty($filteredDestinations)) { ?>
+            <?= count($filteredDestinations) ?> voyages correspondent à vos critères
+        <?php } else { ?>
+            Nous n'avons trouvé aucun voyage correspondant à vos critères
+        <?php } ?>
+    </h2>
+
+    <p class="formtrip-textright">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ratione molestiae, commodi veniam,
+        dolores non hic deleniti blanditiis explicabo aut, voluptatibus labore vel atque reiciendis velit mollitia rerum maxime delectus.
+    </p>
 
 
     <!----------------
---   The cards   --
+    --   The cards   --
 ------------------>
 
-    <?php
+    <div class='form-trip-page'>
+        <?php foreach ($filteredDestinations as $destination => $content) {  ?>
+            <div class='picturesContainer'>
+                <?= $content['name'] ?>
+                <?= $content['image'] ?>
+                <?= $content['description'] ?>
 
-    echo "<div class='form-trip-page'>";
+                <div class="criterias">
+                    <p>
+                        Dépaysement : <?= $content['changeOfScenery'] ?>
+                    </p>
 
-    $x = 0;
-    foreach ($destinations as $destination => $content) {
-        if ($wheather == $content['wheather'] && $depay == $content['changeOfScenery'] && $budget >= $content['costByDay'] * $duree * $person && $x < 5) {
+                    <p>
+                        Climat : <?= $content['wheather'] ?>
+                    </p>
 
-            $total = ($content['costByDay'] * $person * $duree);
-            echo "<div class='picturesContainer'>" .
-                $content['name'] .
-                $content['image'] .
-                $content['description'] .
+                    <p>
+                        Par jour/pers : <?= $content['costByDay'] ?> €
+                    </p>
 
-                "<p>Dépaysement: {$content['changeOfScenery']}</br>
-                            Climat: {$content['wheather']} </br>
-                            Par jour/pers: {$content['costByDay']} €</br>
-                            <b>Coût total: {$total} € </b></p>" .
-                "</div>";
-            $x++;
-        }
-    }
-
-    echo "</div>";
-    ?>
+                    <?php if (!empty($content['total'])) { ?>
+                        <p>
+                            <b>Coût total : <?= $content['total'] ?> €</b>
+                        </p>
+                    <?php } ?>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
 </div>
-<?php
-include("footer.php");
-?>
+
+<?php include("footer.php"); ?>
